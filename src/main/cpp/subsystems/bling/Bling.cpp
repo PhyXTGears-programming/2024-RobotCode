bdb958adbce8496b6e75dada0875d3e595231f12
Ethan Kirk <nott3ch@gmail.com>
2024-02-13 18:44:56 -0500

    Add bling subsystem
    - Ethan K

---
+++ src/main/cpp/subsystems/bling/Bling.cpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "subsystems/bling/Bling.h"
+
+#include <frc2/command/SubsystemBase.h>
+
+BlingSubsystem::BlingSubsystem() {}
---
+++ src/main/include/subsystems/bling/Bling.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+
+class BlingSubsystem : public frc2::SubsystemBase {
+    public:
+
+    private:
+};
