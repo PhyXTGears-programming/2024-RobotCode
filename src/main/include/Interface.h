abc07c52b09fbe4cdd6f5e8c8fe48506489522fc
PhyXTGears <phyxtgearspro@gmail.com>
2024-02-17 14:39:36 -0500

    Add interface header
    - Justin C

---
+++ src/main/include/Interface.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Interface {
+    namespace {
+        const int k_can00 = 0;
+
+        const int k_can70 = 70;
+        const int k_can71 = 71;
+
+        const int k_dio0 = 0;
+
+        const int k_pwm0 = 0;
+    }
+
+    const int k_speakerShooterMotor1 = k_can70;
+    const int k_speakerShooterMotor2 = k_can71;
+
+    const int k_speakerShooterNoteSensor = k_dio0;
+}
