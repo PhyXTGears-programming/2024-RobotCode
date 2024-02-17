16088875cee6a02d9923c3729a6f7b67e7a9cf8b
PhyXTGears <phyxtgearspro@gmail.com>
2024-02-17 14:51:33 -0500

    Initial code for speaker shooter
    - Jaxon G

--- src/main/cpp/subsystems/speaker_shooter/SpeakerShooter.cpp
+++ src/main/cpp/subsystems/speaker_shooter/SpeakerShooter.cpp
@@ -2,4 +2,63 @@
 
 #include <frc2/command/SubsystemBase.h>
 
-SpeakerShooterSubsystem::SpeakerShooterSubsystem() {}
+SpeakerShooterSubsystem::SpeakerShooterSubsystem(std::shared_ptr<cpptoml::table> table)
+    : m_shootMotor1(
+        Interface::k_speakerShooterMotor1,
+        rev::CANSparkMaxLowLevel::MotorType::kBrushless
+    ),
+    m_shootPid1(m_shootMotor1.GetPIDController()),
+    m_shootEncoder1(m_shootMotor1.GetEncoder(
+        rev::SparkRelativeEncoder::Type::kHallSensor
+    )),
+    m_shootMotor2(
+        Interface::k_speakerShooterMotor2,
+        rev::CANSparkMaxLowLevel::MotorType::kBrushless
+    ),
+    m_noteSensor(Interface::k_speakerShooterNoteSensor)
+{
+    // Load configuration values from TOML.
+    {
+        cpptoml::option<double> speed = table->get_qualified_as<double>("shootSpeed");
+
+        if (!speed) {
+            throw "Error: arm shooter cannot find toml property armShooter.shootSpeed";
+        }
+
+        m_config.shootSpeed = rpm_t(*speed);
+    }
+
+    {
+        cpptoml::option<double> speed = table->get_qualified_as<double>("reverseSpeed");
+
+        if (!speed) {
+            throw "Error: arm shooter cannot find toml property armShooter.reverseSpeed";
+        }
+
+        m_config.reverseSpeed = rpm_t(*speed);
+    }
+
+    m_shootPid1.SetFeedbackDevice(m_shootEncoder1);
+
+    // Shoot motor 2 shall follow motor 1 in reverse direction.
+    m_shootMotor2.Follow(m_shootMotor1, true);
+
+}
+
+void SpeakerShooterSubsystem::Shoot() {
+    SetShooterSpeed(m_config.shootSpeed);
+}
+
+void SpeakerShooterSubsystem::ReverseShooter() {
+    SetShooterSpeed(m_config.reverseSpeed);
+}
+
+void SpeakerShooterSubsystem::StopShooter() {
+    SetShooterSpeed(0_rpm);
+}
+
+bool SpeakerShooterSubsystem::IsNoteDetected() {
+    return m_noteSensor.Get();
+}
+
+bool SpeakerShooterSubsystem::IsSpeakerNear() {}
--- src/main/include/subsystems/speaker_shooter/SpeakerShooter.h
+++ src/main/include/subsystems/speaker_shooter/SpeakerShooter.h
@@ -1,10 +1,55 @@
-#pragma once
+#pragma once //makes it so that parent can only be used once
 
+/*******************************
+ a parent always has a child a parent in coding also has a
+ child example of a parent: animal is the parent of dog
+ this would also applie to coding so (line of code)
+ is parent of (other line of code)
+*******************************/
+
+#include "external/cpptoml.h"
+#include "Interface.h"
+
+#include <frc/DigitalInput.h>
 #include <frc2/command/SubsystemBase.h>
 
-class SpeakerShooterSubsystem : public frc2::SubsystemBase {
+#include <units/angular_velocity.h>
+#include <units/length.h>
+
+#include <rev/CANSparkMax.h>
+
+using rpm_t = units::revolutions_per_minute_t;
+
+class SpeakerShooterSubsystem : public frc2::SubsystemBase { //SubsystemBase is the Parent of SpeakerShooterSubsystem
     public:
-        SpeakerShooterSubsystem();
+        SpeakerShooterSubsystem(std::shared_ptr<cpptoml::table> table);
+
+        void Shoot();
+        void ReverseShooter();
+        void StopShooter();
+
+        bool IsNoteDetected();
+
+        rpm_t GetShooterSpeed();
+
+        void SetShooterSpeed(rpm_t speed);
+
+        units::meter_t GetSpeakerDistance();
+        bool IsSpeakerNear();
 
     private:
+    
+        rev::CANSparkMax m_shootMotor1;
+        rev::SparkPIDController m_shootPid1;
+        rev::SparkRelativeEncoder m_shootEncoder1;
+
+        rev::CANSparkMax m_shootMotor2;
+
+        frc::DigitalInput m_noteSensor;
+
+        // Config settings loaded from TOML.
+        struct {
+            rpm_t shootSpeed;
+            rpm_t reverseSpeed;
+        } m_config;
 };
