1592ae70f367b46979291c6cf6808f748d0c2831
PhyXTGears <phyxtgearspro@gmail.com>
2024-03-06 12:32:42 -0500

    Add DriveTeleopCommand
    - Dominic A

---
+++ src/main/cpp/commands/DriveTeleopCommand.cpp
@@ -0,0 +1,84 @@
+#include "commands/DriveTeleopCommand.h"
+#include "subsystems/drivetrain/Drivetrain.h"
+
+#include "Constants.h"
+#include "util/math.h"
+
+#include <frc/XboxController.h>
+#include <frc/filter/SlewRateLimiter.h>
+#include <frc/MathUtil.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+#include <frc/shuffleboard/Shuffleboard.h>
+#include <frc/shuffleboard/ShuffleboardTab.h>
+#include <frc/shuffleboard/ShuffleboardLayout.h>
+#include <networktables/GenericEntry.h>
+
+#include <units/angular_velocity.h>
+
+#define JOYSTICK_DEADZONE 0.2
+
+using units::meters_per_second_t;
+using units::radians_per_second_t;
+
+DriveTeleopCommand::DriveTeleopCommand(
+    Drivetrain* drivetrain,
+    frc::XboxController* driveController,
+    units::second_t period
+) {
+    AddRequirements(drivetrain);
+
+    m_driveController = driveController;
+    m_drivetrain = drivetrain;
+    m_robotPeriod = period;
+}
+
+void DriveTeleopCommand::Initialize() {
+    m_drivetrain->UpdateOdometry();
+    //compresses the range of the driving speed to be within the max speed and
+    //the minimum. but have the normal speed be the default if no trigger is 
+    //being pressed (so both register 0)
+    //
+    //NOTE no trigger takes priority of the other so if both pressed they will cancel each other 
+    double leftTrigger = m_driveController->GetLeftTriggerAxis();   // Range [0.0..1.0]
+    double rightTrigger = m_driveController->GetRightTriggerAxis(); // Range [0.0..1.0]
+
+    meters_per_second_t driveReduce = leftTrigger * (Constants::k_normalDriveSpeed - Constants::k_slowDriveSpeed);
+    meters_per_second_t driveGain   = rightTrigger * (Constants::k_fastDriveSpeed - Constants::k_normalDriveSpeed);
+
+    meters_per_second_t driveSpeedFactor = Constants::k_normalDriveSpeed + driveGain - driveReduce;
+
+    radians_per_second_t turnReduce = leftTrigger * (Constants::k_maxTurnSpeed - Constants::k_slowTurnSpeed);
+
+    radians_per_second_t turnSpeedFactor = Constants::k_maxTurnSpeed - turnReduce;
+
+    // the rotation limit is there in case the driver does not want to spin as fast while driving
+    const auto forwardSpeed = driveSpeedFactor
+        * m_forwardSpeedLimiter.Calculate(frc::ApplyDeadband(
+            -m_driveController->GetLeftY(),
+            JOYSTICK_DEADZONE)
+        );
+
+    const auto strafeSpeed = driveSpeedFactor
+        * m_strafeSpeedLimiter.Calculate(frc::ApplyDeadband(
+            -m_driveController->GetLeftX(),
+            JOYSTICK_DEADZONE)
+        );
+
+    const auto turnSpeed = turnSpeedFactor
+        * m_turnSpeedLimiter.Calculate(frc::ApplyDeadband(
+            m_driveController->GetRightX(),
+            JOYSTICK_DEADZONE)
+        );
+
+    m_drivetrain->Drive(forwardSpeed, strafeSpeed, turnSpeed, true, m_robotPeriod);
+}
+
+void DriveTeleopCommand::End(bool interrupted) {
+    // Stop the drive motors!
+    m_drivetrain->Drive(0_mps, 0_mps, 0_rad_per_s, true, m_robotPeriod);
+}
+
+bool DriveTeleopCommand::IsFinished() {
+    return false; //dont end because then we wont be able to drive
+}
---
+++ src/main/include/commands/DriveTeleopCommand.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "subsystems/drivetrain/Drivetrain.h"
+
+#include <units/time.h>
+
+#include <frc/filter/SlewRateLimiter.h>
+#include <frc/XboxController.h>
+
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+
+class DriveTeleopCommand : public frc2::CommandHelper<frc2::CommandBase, DriveTeleopCommand> {
+    public:
+        DriveTeleopCommand(
+            Drivetrain * drivetrain,
+            frc::XboxController * driverController,
+            units::second_t period = 20_ms
+        );
+
+        void Initialize() override;
+        void Execute() override;
+        void End(bool interrupted) override;
+        bool IsFinished() override;
+
+    private:
+        units::second_t m_robotPeriod;
+
+        Drivetrain * m_drivetrain;
+
+        frc::XboxController * m_driveController;
+
+        frc::SlewRateLimiter<units::scalar> m_forwardSpeedLimiter{ 3/1_s };
+        frc::SlewRateLimiter<units::scalar> m_strafeSpeedLimiter{ 3/1_s };
+        frc::SlewRateLimiter<units::scalar> m_turnSpeedLimiter{ 3/1_s };
+};
