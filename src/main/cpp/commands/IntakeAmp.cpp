04c68dfb8ff814bb1637bd219b356bb6ebb9347d
PhyXTGears <phyxtgearspro@gmail.com>
2024-03-07 19:40:20 -0500

    Add IntakeAmp command
    - Weston J

---
+++ src/main/cpp/commands/IntakeAmp.cpp
@@ -0,0 +1,28 @@
+
+#include "commands/IntakeAmp.h"
+
+IntakeAmp::IntakeAmp(IntakeSubsystem * intake, AmpShooterSubsystem * amp) {
+    AddRequirements(intake);
+    AddRequirements(amp);
+
+    m_amp = amp;
+    m_intake = intake;
+
+    
+}
+
+void IntakeAmp::Initialize() {
+
+}
+
+void IntakeAmp::Execute() {
+    m_intake->IntakeAmpShooter();
+}
+
+void IntakeAmp::End(bool interrupted) {
+    m_intake->Stop();
+}
+
+bool IntakeAmp::IsFinished() {
+    return m_amp->IsNoteDetected();
+}
