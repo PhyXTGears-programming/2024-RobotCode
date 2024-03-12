d4ad3624fb46a1faf97d13e1ff055ed684e52eac
PhyXTGears <phyxtgearspro@gmail.com>
2024-03-12 03:31:21 -0400

    Calibrate swerve drive speeds
    Preliminary calibration.  Repeat if necessary with input from drive
    team.
    
    - Justin C

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -19,13 +19,13 @@ using rad_per_sec_per_volt_t = units::unit_t<rad_per_sec_per_volt>;
 
 namespace constants {
 
-    constexpr meters_per_second_t k_maxDriveSpeed = 3_mps;
+    constexpr meters_per_second_t k_maxDriveSpeed = 5_mps;
 
-    constexpr meters_per_second_t k_normalDriveSpeed = 1_mps;
-    constexpr meters_per_second_t k_slowDriveSpeed = k_normalDriveSpeed * (1.0 - 0.20); // 20% slower.
-    constexpr meters_per_second_t k_fastDriveSpeed = k_normalDriveSpeed * (1.0 + 0.25); // 25% faster.
+    constexpr meters_per_second_t k_normalDriveSpeed = 1.5_mps;
+    constexpr meters_per_second_t k_slowDriveSpeed = k_normalDriveSpeed * (1.0 - 0.75); // 75% slower.
+    constexpr meters_per_second_t k_fastDriveSpeed = k_normalDriveSpeed * (1.0 + 0.75); // 75% faster.
 
-    constexpr radians_per_second_t k_maxTurnSpeed = std::numbers::pi * 1_rad_per_s;
+    constexpr radians_per_second_t k_maxTurnSpeed = 180_deg_per_s;
 
     constexpr radians_per_second_t k_slowTurnSpeed = k_maxTurnSpeed * (1.0 - 0.5);  // 50% slower.
 
