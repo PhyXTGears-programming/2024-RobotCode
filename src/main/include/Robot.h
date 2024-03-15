445438ce8c9673cb4d52145dddfe990fd1001317
PhyXTGears <phyxtgearspro@gmail.com>
2024-03-15 11:15:08 -0400

    Retract amp swivel so it don't snag
    - Justin C

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -55,6 +55,9 @@ class Robot : public frc::TimedRobot {
         frc2::CommandPtr m_closeGate = frc2::cmd::None();
         frc2::CommandPtr m_driveTeleopCommand = frc2::cmd::None();
         frc2::CommandPtr m_openGate = frc2::cmd::None();
+
+        frc2::CommandPtr m_retractAmp = frc2::cmd::None();
+
         frc2::CommandPtr m_intakeSpeaker = frc2::cmd::None();
         frc2::CommandPtr m_reverseSpeaker = frc2::cmd::None();
         frc2::CommandPtr m_shootSpeaker = frc2::cmd::None();
